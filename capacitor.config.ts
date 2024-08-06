96e3c4c80ea6f8932f8a55b956ad5f1333a5cb31
LeooRogelioSilva <leonardo.silva@vrsoft.com.br>
2024-08-05 23:05:46 -0300

    Hello World

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'PenguinBudget',
-  webDir: 'www'
-};
-
-export default config;
